975974678919682861f064dbb3b2af8e22b23370
Khatermm <lawicki02@gmail.com>
2022-11-11 19:52:53 +0100

    png_to_p1+some_upgades

--- main.cpp
+++ main.cpp
@@ -1,73 +1,98 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-#include <unistd.h>
+#include <stdlib.h>
+#include "windows.h"
+//#include <unistd.h>
 
 using namespace std;
 
 fstream plik;
 
-void wypiszobraz( int row , int col ){
+void wypiszobraz(int row, int col)
+{
     char c[1];
-    for (int i =0 ; i < row ; i++){
-        for (int j = 0 ; j < col ; j++){
-            //c = " ";
-            plik.read(c , 1);
-            if ((c == " ")||(c == ""))   {
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            // c = " ";
+            plik.read(c, 1);
+            if ((c == " ") || (c == ""))
+            {
                 continue;
             }
-            else{
-                if (c[0] == '0'){
-                    std::cout<<" ";
+            else
+            {
+                if (c[0] == '0')
+                {
+                    std::cout << " ";
                 }
                 else if (c[0] == '1')
                 {
-                    int v = rand()%2;
+                    int v = rand() % 2;
                     std::cout << v;
                 }
-                else {
-                    std::cout << c[0];
+                // else if (c[0] == ' ')
+                //{
+                //     continue;
+                // }
+                else
+                {
+                    cout << c[0];
                 }
-            
             }
 
-            //usleep( 1 );
-            
+            // usleep( 1 );
         }
-        //cout<<endl;
+        // cout<<endl;
     }
-    //std::cout << endl << c[0] << endl ;  
+    // std::cout << endl << c[0] << endl ;
 }
 
-int main(){
+int main()
+{
     int n = 0;
-    plik.open( "obraz.pbm", ios::in | ios::binary );
-    if( plik.good() == true ){
-        //std::cout << "Uzyskano dostep do pliku!" << std::endl;
+    plik.open("obraz.pbm", ios::in | ios::binary);
+    if (plik.good() == true)
+    {
+        // std::cout << "Uzyskano dostep do pliku!" << std::endl;
 
         char ss1[255];
         char ss2[255];
-        //char c[1];
+        // char c[1];
 
-
-        while (n<100){
+        while (n < 10)
+        {
 
             plik.getline(ss1, 255);
             plik.getline(ss2, 255);
-        
-            //std::cout << ss1 << endl << ss2 << endl ;
 
-            system("clear");
-            
-            wypiszobraz(7 , 49 );
-            usleep( 100000 );
+            // int row;
+            // int col;
+
+            // for (int i = 0; i < 255; i++)
+            // {
+            //     if (ss2[i] == " ")
+            //     {
+            //     }
+            // }
+
+            // std::cout << ss1 << endl << ss2 << endl ;
+
+            system("CLS");
+
+            wypiszobraz(50, 50 * 2);
+            // usleep(100000);
+            Sleep(100);
             plik.seekg(0, plik.beg);
             n++;
         }
 
-
-        std::cout<<endl;     
-    }    else std::cout << "ops!" << std::endl;
+        std::cout << endl;
+    }
+    else
+        std::cout << "ops!" << std::endl;
 
     return 0;
 }
